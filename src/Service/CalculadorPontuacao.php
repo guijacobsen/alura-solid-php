688b6180b05fa955f9e7d76361a66d8a0e3abe43
Guilherme Jacobsen <guijacobsen@gmail.com>
2021-03-05 21:58:41 -0300

    WIP - Aplicando Open Closed Principle - Criada interface IPontuavel para que seja implementada em classes que venham a ter pontuação - Cada classe implementa sua própria regra

--- src/Service/CalculadorPontuacao.php
+++ src/Service/CalculadorPontuacao.php
@@ -2,19 +2,13 @@
 
 namespace Alura\Solid\Service;
 
-use Alura\Solid\Model\AluraMais;
-use Alura\Solid\Model\Curso;
+use Alura\Solid\Model\IPontuavel;
 
 class CalculadorPontuacao
 {
-    public function recuperarPontuacao($conteudo)
+    public function recuperarPontuacao(IPontuavel $conteudo)
     {
-        if ($conteudo instanceof Curso) {
-            return 100;
-        } else if ($conteudo instanceof AluraMais) {
-            return $conteudo->minutosDeDuracao() * 2;
-        } else {
-            throw new \DomainException('Apenas Cursos e videos Alura+ possuem pontuações');
-        }
+        return $conteudo->recuperarPontuacao();
+        
     }
 }
